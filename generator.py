9ef889506c741d339d3322a407f36eab6135abbc
Deceptrax123 <srinitishsri@gmail.com>
2024-05-30 00:11:52 +0530

    complete synthesis network

--- generator.py
+++ generator.py
@@ -1,6 +1,47 @@
 import torch
 from torch.autograd import Variable
-from torch.nn import Linear, Conv2d, ConvTranspose2d, ReLU, Module, BatchNorm1d
+from torch.nn import Linear, Conv2d, Module, BatchNorm1d, UpsamplingBilinear2d
+
+
+class Generator(Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.f = MappingNetwork()
+        self.g1 = SynthesisNetwork(in_channels=512, out_channels=256, noise_shape1=(
+            512, 4, 4), noise_shape2=(256, 8, 8))
+        self.g2 = SynthesisNetwork(in_channels=256, out_channels=128, noise_shape1=(
+            256, 8, 8), noise_shape2=(128, 16, 16))
+        self.g3 = SynthesisNetwork(in_channels=128, out_channels=64, noise_shape1=(
+            128, 16, 16), noise_shape2=(64, 32, 32))
+        self.g4 = SynthesisNetwork(in_channels=64, out_channels=32, noise_shape1=(
+            64, 32, 32), noise_shape2=(32, 64, 64))
+        self.g5 = SynthesisNetwork(in_channels=32, out_channels=16, noise_shape1=(
+            32, 64, 64), noise_shape2=(16, 128, 128))
+        self.g6 = SynthesisNetwork(in_channels=16, out_channels=8, noise_shape1=(
+            16, 128, 128), noise_shape2=(8, 256, 256))
+        self.g7 = SynthesisNetwork(in_channels=8, out_channels=4, noise_shape1=(
+            8, 256, 256), noise_shape2=(4, 512, 512))
+        self.g8 = SynthesisNetwork(in_channels=4, out_channels=3, noise_shape1=(
+            4, 512, 512), noise_shape2=(4, 1024, 1024))
+
+        self.conv = Conv2d(in_channels=4, out_channels=3,
+                           kernel_size=(1, 1), stride=1)
+
+    def forward(self, z, constant):
+        f = self.f(z)
+        x = self.g1(f, constant)
+        x = self.g2(f, x)
+        x = self.g3(f, x)
+        x = self.g4(f, x)
+        x = self.g5(f, x)
+        x = self.g6(f, x)
+        x = self.g7(f, x)
+        x = self.g8(f, x)
+
+        x = self.conv(x)
+
+        return x
 
 
 class MappingNetwork(Module):
@@ -19,14 +60,15 @@ class MappingNetwork(Module):
         self.linear8 = Linear(in_features=256, out_features=512)
 
     def forward(self, x):
-        x = self.linear1(x)
-        x = self.linear2(x)
-        x = self.linear3(x)
-        x = self.linear4(x)
-        x = self.linear5(x)
-        x = self.linear6(x)
-        x = self.linear7(x)
-        x = self.linear8(x)
+        x = self.bn(x)
+        x = self.linear1(x).relu()
+        x = self.linear2(x).relu()
+        x = self.linear3(x).relu()
+        x = self.linear4(x).relu()
+        x = self.linear5(x).relu()
+        x = self.linear6(x).relu()
+        x = self.linear7(x).relu()
+        x = self.linear8(x).relu()
 
         return x
 
@@ -40,8 +82,7 @@ class SynthesisNetwork(Module):
         self.ada3 = AdaIn()
         self.ada4 = AdaIn()
 
-        self.upsample = ConvTranspose2d(in_channels=in_channels, out_channels=out_channels,
-                                        stride=2, padding=1, output_padding=1, kernel_size=(3, 3))
+        self.upsample = UpsamplingBilinear2d(size=(2, 2), scale_factor=2)
 
         self.conv1 = Conv2d(in_channels=in_channels, out_channels=in_channels,
                             stride=1, kernel_size=(3, 3), padding=1)
